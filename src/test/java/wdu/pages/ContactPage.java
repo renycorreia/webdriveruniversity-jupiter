bbf1d433eee0617aa2cd6cd5fd6f0102eb9645f1
Reny Correia <renylmg@gmail.com>
2024-04-11 09:55:00 -0300

    Inclusão de cenário de sucesso na feature Contato | Melhoria/reaproveitamento de código

--- src/test/java/wdu/pages/ContactPage.java
+++ src/test/java/wdu/pages/ContactPage.java
@@ -3,6 +3,10 @@ package wdu.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import wdu.domain.Pessoa;
+import wdu.utils.GenericsUtils;
+
+import static wdu.utils.GenericsUtils.retornaPessoa;
 
 public class ContactPage {
     private static WebElement element = null;
@@ -27,14 +31,77 @@ public class ContactPage {
         return element;
     }
 
+    public static WebElement btnReset(WebDriver driver) {
+        element = driver.findElement(By.cssSelector("[value='RESET']"));
+        return element;
+    }
+
     public static WebElement btnSubmit(WebDriver driver) {
         element = driver.findElement(By.cssSelector("[value='SUBMIT']"));
         return element;
     }
 
-    public static WebElement tagBody(WebDriver driver) {
+    public static WebElement tagBodyErro(WebDriver driver) {
         element = driver.findElement(By.tagName("body"));
         return element;
+    }
+
+    public static WebElement tagH1Sucesso(WebDriver driver) {
+        element = driver.findElement(By.tagName("h1"));
+        return element;
+    }
+
+    public static void preencheCampos(WebDriver driver, String nomeCampo)
+    {
+        switch (nomeCampo){
+            case "First Name":
+                preencheNome(driver, GenericsUtils.retornaPessoa().getPrimeiroNome());
+                break;
+            case "Last Name":
+                preencheSobrenome(driver, GenericsUtils.retornaPessoa().getUltimoNome());
+                break;
+            case "Email Address":
+                preencheEmail(driver, GenericsUtils.retornaPessoa().getEmail());
+                break;
+            case "Comments":
+                preencheComentario(driver, GenericsUtils.faker.lorem().fixedString(100));
+                break;
+        }
+    }
+
+    public static void preencheNome(WebDriver driver, String valor)
+    {
+        cmpFirstName(driver).sendKeys(valor);
+    }
+
+    public static void preencheSobrenome(WebDriver driver, String valor)
+    {
+        cmpLastName(driver).sendKeys(valor);
+    }
+
+    public static void preencheEmail(WebDriver driver, String valor)
+    {
+        cmpEmail(driver).sendKeys(valor);
+    }
+
+    public static void preencheComentario(WebDriver driver, String valor)
+    {
+        cmpComment(driver).sendKeys(valor);
+    }
+
+    public static void preencheTodosOsCampos(WebDriver driver)
+    {
+        Pessoa p = retornaPessoa();
+
+        preencheNome(driver, p.getPrimeiroNome());
+        preencheSobrenome(driver, p.getUltimoNome());
+        preencheEmail(driver, p.getEmail());
+        preencheComentario(driver, GenericsUtils.faker.lorem().fixedString(100));
+    }
+
+    public static void limpaFormulario(WebDriver driver)
+    {
+        btnReset(driver).click();
     }
 
 }
